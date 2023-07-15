2387dcaaea62aa407431a5229fe8dfd8d0c092bd
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-07-15 09:10:36 +0300

    update

--- Algorithms/index.js
+++ Algorithms/index.js
@@ -64,6 +64,97 @@ const anotherIsPrime = (n) => {
     }
     return true;
   }
-}
+};
 
 ///////////////////////////////////////////////////////////
+
+//Power of two
+const isPowerOfTwo = (n) => {
+  const res = Math.log(n) / Math.log(2);
+  return Number.isInteger(res);
+};
+
+//Another power of two
+const anotherIsPowerOfTwo = (n) => {
+  if (n < 1) return false;
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n /= 2;
+  }
+  return true;
+};
+
+//BitWise power of two
+const bitWiseIsPowerOfTwo = (n) => {
+  if (n < 1) return false;
+  return (n & (n - 1)) === 0;
+};
+
+//////////////////////////////////////////////
+
+//Recursion
+
+//Recursive fibonacci
+const recFib = (n) => {
+  if (n < 2) return n;
+  return recFib(n - 1) + recFib(n - 2);
+};
+
+//Recursive factorial
+const recFac = (n) => {
+  if (n === 1) return 1;
+  return n * recFac(n - 1);
+};
+
+//////////////////////////////////////////////
+
+//Search Algorithms
+
+//Linear seacrch O(n)
+const linearSearch = (arr, n) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === n) return i;
+  }
+  return -1;
+};
+
+//Binary search (work only on sorted array) O(log(n))
+const binarySearch = (arr, t) => {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (t === arr[middleIndex]) return middleIndex;
+
+    if (t < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+
+  return -1;
+};
+
+//Sorting alogorithms
+
+// Quick sort O(nlog(n))
+const quickSort = (arr) => {
+  if(arr.length < 2) return arr;
+  let pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > pivot) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+console.log(quickSort([8, 20, -2, 4, -6]));
