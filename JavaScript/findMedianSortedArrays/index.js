d350d1882c8046cbe9718d56b988d0be5a09b461
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-09-21 11:52:40 +0300

    ch

---
+++ JavaScript/findMedianSortedArrays/index.js
@@ -0,0 +1,36 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+  if (nums1.length > nums2.length) {
+    [nums1, nums2] = [nums2, nums1];
+}
+
+const m = nums1.length;
+const n = nums2.length;
+let low = 0, high = m;
+
+while (low <= high) {
+    const partitionX = Math.floor((low + high) / 2);
+    const partitionY = Math.floor((m + n + 1) / 2) - partitionX;
+    
+    const maxX = (partitionX === 0) ? Number.MIN_SAFE_INTEGER : nums1[partitionX - 1];
+    const maxY = (partitionY === 0) ? Number.MIN_SAFE_INTEGER : nums2[partitionY - 1];
+    
+    const minX = (partitionX === m) ? Number.MAX_SAFE_INTEGER : nums1[partitionX];
+    const minY = (partitionY === n) ? Number.MAX_SAFE_INTEGER : nums2[partitionY];
+    
+    if (maxX <= minY && maxY <= minX) {
+        if ((m + n) % 2 === 0) {
+            return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+        } else {
+            return Math.max(maxX, maxY);
+        }
+    } else if (maxX > minY) {
+        high = partitionX - 1;
+    } else {
+        low = partitionX + 1;
+    }
+}
+
+throw new Error("Input arrays are not sorted.");
+};
+
+console.log(findMedianSortedArrays([1,3], [2]))
