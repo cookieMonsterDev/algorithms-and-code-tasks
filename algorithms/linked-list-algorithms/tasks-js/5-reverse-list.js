0aab092fb3fba7980882275b047709c54c2f972c
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-11-17 13:27:53 +0200

    ch

--- algorithms/linked-list-algorithms/tasks-js/5-reverse-list.js
+++ algorithms/linked-list-algorithms/tasks-js/5-reverse-list.js
@@ -18,3 +18,10 @@ const reverseList = (head) => {
 };
 
 //OR with recursion:
+
+const reverseListRec = (head, prev = null) => {
+  if(head === null) return prev;
+  const next = head.next;
+  head.next = prev;
+  return reverseList(next, head);
+};
