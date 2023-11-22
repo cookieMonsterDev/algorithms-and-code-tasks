3e396d894ea8ab0e5efc6fb2be075443e0795906
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-11-22 09:25:51 +0200

    ch

---
+++ algorithms/binary-tree/tasks-js/1-depth-first-values.js
@@ -0,0 +1,34 @@
+/**
+ * depth first values
+
+Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
+
+Hey. This is our first binary tree problem, so be extra sure to check out the approach video! -AZ
+ */
+
+const depthFirstValues = (root) => {
+  if(!root) return [];
+  
+  const values = [];
+  
+  const stuck = [root];
+  
+  while(stuck.length > 0) {
+    const node = stuck.pop();
+    
+    values.push(node.val);
+    
+    if(node.right) stuck.push(node.right);
+    if(node.left) stuck.push(node.left);
+  }
+  
+  return values;
+};
+
+//With recursion:
+
+const depthFirstValuesRec = (root) => {
+  const leftValues = depthFirstValues(root.left);
+   const rightValues = depthFirstValues(root.right);
+   return [ root.val, ...leftValues, ...rightValues ];
+ }
